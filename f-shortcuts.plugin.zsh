9ad1293547f7c3351e47e8fcf7250a63a547e4f8
Grigorii Horos <horosgrisa@gmail.com>
2019-05-19 10:33:38 +0300

    Fix README.md

--- f-shortcuts.plugin.zsh
+++ f-shortcuts.plugin.zsh
@@ -4,6 +4,10 @@ if [[ ! -e ~/.zsh-f-shortcuts.yaml ]]; then
   cp ${0:A:h}/zsh-f-shortcuts.example.yaml ~/.zsh-f-shortcuts.yaml
 fi
 
+if [[! -e /tmp/zpm-zsh-f-shorcuts.$USER.zsh ]]; then
+  ruby ${0:A:h}/generate.rb
+fi
+
 if [[ ~/.zsh-f-shortcuts.yaml -nt /tmp/zpm-zsh-f-shorcuts.$USER.zsh ]]; then
   ruby ${0:A:h}/generate.rb
 fi
