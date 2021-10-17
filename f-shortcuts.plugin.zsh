8fce777d50f3fe71aa6e34296c3afd39db974f10
Spektrum-interface <marcomariocostantino64@gmail.com>
2021-10-17 11:08:56 +0200

    added prefix and made it POSIX

--- f-shortcuts.plugin.zsh
+++ f-shortcuts.plugin.zsh
@@ -1,14 +1,20 @@
 source ${0:A:h}/functions.zsh
 
-if [[ ! -e ~/.zsh-f-shortcuts.yaml ]]; then
-  cp ${0:A:h}/zsh-f-shortcuts.example.yaml ~/.zsh-f-shortcuts.yaml
+if [ $ZDOTDIR ]; then
+  export _PREFIX=$ZDOTDIR
+else
+  export _PREFIX="~/"
 fi
 
-if [[ ! -e /tmp/zpm-zsh-f-shorcuts.$USER.zsh ]]; then
+if [ ! -e $_PREFIX/.zsh-f-shortcuts.yaml ]; then
+  cp ${0:A:h}/zsh-f-shortcuts.example.yaml $_PREFIX/.zsh-f-shortcuts.yaml
+fi
+
+if [ ! -e /tmp/zpm-zsh-f-shorcuts.$USER.zsh ]; then
   ruby ${0:A:h}/generate.rb
 fi
 
-if [[ ~/.zsh-f-shortcuts.yaml -nt /tmp/zpm-zsh-f-shorcuts.$USER.zsh ]]; then
+if [ $_PREFIX/.zsh-f-shortcuts.yaml -nt /tmp/zpm-zsh-f-shorcuts.$USER.zsh ]; then
   ruby ${0:A:h}/generate.rb
 fi
 
